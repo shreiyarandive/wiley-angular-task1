0d98f1a497ffb5f880cb325b2e179b6b47deda6f
Shreiya Randive <shreiyarandive1@gmail.com>
2022-04-08 13:08:07 +0530

    two-way-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NameDisplayComponent } from './name-display/name-display.component';
 import { AgeDisplayComponent } from './age-display/age-display.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameDisplayComponent,
-    AgeDisplayComponent
+    AgeDisplayComponent,
+    TwoWayBindingComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
